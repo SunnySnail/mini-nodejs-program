4c6d0ac9ff349155ef2613a4441d341ac3dc8094
huangxiaoyan <hxysnail@gmail.com>
2019-11-06 15:32:51 +0800

    update

--- database/nedb/count.js
+++
@@ -1,7 +0,0 @@
-var db = require('./db.js')
-
-db.count({
-  age: 20
-}, function(err, docs) {
-  console.log(docs)
-})
--- database/nedb/db.js
+++
@@ -1,7 +0,0 @@
-var Datastore = require('nedb')
-var db = new Datastore({
-  filename: './user.db',
-  autoload: true
-})
-
-module.exports = db
